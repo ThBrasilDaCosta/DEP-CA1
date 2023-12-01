180aa844b3ad093c2d3f387b58dc1d1dd7fa1cca
2020005 <thiagoocosta86@gmail.com>
2023-12-01 12:41:06 +0000

    started manipulating dataset, created a subset for crimes in 2018. Current size (65685,17).

--- r_code.R
+++ r_code.R
@@ -2,3 +2,7 @@ library(readr)
 #reading csv file from big data set
 CrimesinBoston <- read_csv("CrimesinBoston.csv")
 View(CrimesinBoston)
+
+#creating subset of crimes in 2018
+crimes_2018 <- subset(CrimesinBoston, YEAR == 2018)
+View(crimes_2018)
