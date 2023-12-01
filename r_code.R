dae903c58bef2f62c1436e2783f417fd1acc5727
2020005 <thiagoocosta86@gmail.com>
2023-12-01 14:07:30 +0000

    Dropping empty column 'SHOOTING' from the subset. Subset now (65685, 16)

--- r_code.R
+++ r_code.R
@@ -6,3 +6,7 @@ View(CrimesinBoston)
 #creating subset of crimes in 2018
 crimes_2018 <- subset(CrimesinBoston, YEAR == 2018)
 View(crimes_2018)
+
+#dropping column 'SHOOTINH' from the subset.
+crimes_2018 <- subset(crimes_2018, select = -SHOOTING)
+View(crimes_2018)
