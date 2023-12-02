c16295938e4277168adbfd50521f1a81edf42b57
2020005 <thiagoocosta86@gmail.com>
2023-12-02 13:25:06 +0000

    trying to change the narrative, the ways was planning to use dataset would be the best.

--- r_code.R
+++ r_code.R
@@ -1,13 +1,18 @@
 library(readr)
 #reading csv file from big data set
 CrimesinBoston <- read_csv("CrimesinBoston.csv")
-View(CrimesinBoston)
-
+#View(CrimesinBoston)#Size 319073, 17
+CrimesinBoston <- subset(CrimesinBoston, select = -c(INCIDENT_NUMBER, OFFENSE_CODE, REPORTING_AREA, SHOOTING, UCR_PART ))
+View(CrimesinBoston)#Size 319073, 12
 #creating subset of crimes in 2017
 crimes_2017 <- subset(CrimesinBoston, YEAR == 2017)
 View(crimes_2017)
 
+
 #dropping unnecessary columns  from the subset.
 crimes_2017 <- subset(crimes_2017, select = -c(INCIDENT_NUMBER, OFFENSE_CODE, REPORTING_AREA, SHOOTING, UCR_PART ))
-View(crimes_2017) 
-#changed subset from 2018 to 2017
+View(crimes_2017) #Size 100886, 12
+
+
+total_crimes17 <- nrow(crimes_2017)
+print(total_crimes17)
