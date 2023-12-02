ce84b6d576dca2618a501e659b775fb118e89f4d
2020005 <thiagoocosta86@gmail.com>
2023-12-02 11:48:35 +0000

    Had to change the subset from year 2018 to 2017 because subset 2018 was going just untill september.

--- r_code.R
+++ r_code.R
@@ -3,10 +3,10 @@ library(readr)
 CrimesinBoston <- read_csv("CrimesinBoston.csv")
 View(CrimesinBoston)
 
-#creating subset of crimes in 2018
-crimes_2018 <- subset(CrimesinBoston, YEAR == 2018)
-View(crimes_2018)
+#creating subset of crimes in 2017
+crimes_2017 <- subset(CrimesinBoston, YEAR == 2017)
+View(crimes_2017)
 
 #dropping unnecessary columns  from the subset.
-crimes_2018 <- subset(crimes_2018, select = -c(INCIDENT_NUMBER, OFFENSE_CODE, REPORTING_AREA, SHOOTING, UCR_PART ))
-View(crimes_2018) 
+crimes_2017 <- subset(crimes_2017, select = -c(INCIDENT_NUMBER, OFFENSE_CODE, REPORTING_AREA, SHOOTING, UCR_PART ))
+View(crimes_2017) 
