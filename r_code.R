5991b30c2e6df483fe12a60dad5d0b95c73671b8
2020005 <thiagoocosta86@gmail.com>
2023-12-02 11:50:54 +0000

    new subset 2017 is (100886, 12)

--- r_code.R
+++ r_code.R
@@ -10,3 +10,4 @@ View(crimes_2017)
 #dropping unnecessary columns  from the subset.
 crimes_2017 <- subset(crimes_2017, select = -c(INCIDENT_NUMBER, OFFENSE_CODE, REPORTING_AREA, SHOOTING, UCR_PART ))
 View(crimes_2017) 
+#changed subset from 2018 to 2017
