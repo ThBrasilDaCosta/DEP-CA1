7105f2cd13ea1319b87fdf8439de0367df1bfb92
2020005 <thiagoocosta86@gmail.com>
2023-12-02 14:41:20 +0000

    brainstorming better way to display and work with dataset.

--- r_code.R
+++ r_code.R
@@ -1,18 +1,24 @@
 library(readr)
-#reading csv file from big data set
+#reading csv file from big data set, Size 319073, 17.
 CrimesinBoston <- read_csv("CrimesinBoston.csv")
-#View(CrimesinBoston)#Size 319073, 17
+#Dropping unnecessary columns.
 CrimesinBoston <- subset(CrimesinBoston, select = -c(INCIDENT_NUMBER, OFFENSE_CODE, REPORTING_AREA, SHOOTING, UCR_PART ))
 View(CrimesinBoston)#Size 319073, 12
-#creating subset of crimes in 2017
+
+#creating subset of crimes 2015, 2016, 2017.
+crimes_2015 <- subset(CrimesinBoston, YEAR == 2015)
+crimes_2016 <- subset(CrimesinBoston, YEAR == 2016)
 crimes_2017 <- subset(CrimesinBoston, YEAR == 2017)
-View(crimes_2017)
-
-
-#dropping unnecessary columns  from the subset.
-crimes_2017 <- subset(crimes_2017, select = -c(INCIDENT_NUMBER, OFFENSE_CODE, REPORTING_AREA, SHOOTING, UCR_PART ))
-View(crimes_2017) #Size 100886, 12
-
-
+crimes_2018 <- subset(CrimesinBoston, YEAR == 2018)
+#using the number of rows in each subset to determine the total number of crimes in each year.
+total_crimes15 <- nrow(crimes_2015)
+total_crimes16 <- nrow(crimes_2016)
 total_crimes17 <- nrow(crimes_2017)
-print(total_crimes17)
+total_crimes18 <- nrow(crimes_2018)
+total_crimesboston <- nrow(CrimesinBoston)
+show(total_crimes15)
+show(total_crimes16)
+show(total_crimes17)
+show(total_crimes18)
+show(total_crimes15 + total_crimes16 + total_crimes17 + total_crimes18)
+show(total_crimesboston)
